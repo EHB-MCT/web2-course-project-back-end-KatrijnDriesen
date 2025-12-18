8ac0d734cec67a5c8d17f33f70d8d75872ba46eb
KatrijnDriesen <115262848+KatrijnDriesen@users.noreply.github.com>
2025-12-18 20:54:11 +0100

    add photo's activiteiten + create get endpoint /eersteActiviteit

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 3000;
 console.log("joehoe");
 
 let carrouselWatGroeitEr;
+let activiteiten;
 
 async function run() {
 	const uri = `mongodb+srv://admin_katrijn:${credentials.password}@wildewei.3etdgyv.mongodb.net/?appName=Wildewei`;
@@ -21,12 +22,19 @@ async function run() {
 
 	const database = client.db("wildewei_databank");
 	carrouselWatGroeitEr = database.collection("watgroeiter");
+	activiteiten = database.collection("activiteiten");
 }
 
 app.get("/watgroeiter", async (req, res) => {
 	const watgroeiterArray = await carrouselWatGroeitEr.find().toArray();
 	console.log(watgroeiterArray);
 	res.send(watgroeiterArray);
+});
+
+app.get("/eersteActiviteit", async (req, res) => {
+	const eersteActiviteit = await activiteiten.find().sort({ startDate: 1 }).limit(1).toArray();
+	console.log(eersteActiviteit);
+	res.send(eersteActiviteit);
 });
 
 app.listen(port, () => {
