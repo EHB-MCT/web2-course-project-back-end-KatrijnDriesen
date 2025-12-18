85f2a551e44ed1946dc8ebb1be54f556210b76ff
KatrijnDriesen <115262848+KatrijnDriesen@users.noreply.github.com>
2025-12-18 22:52:01 +0100

    replace 'get/eersteActiviteit' with general 'get' for 'activiteiten' + add limit for query

--- index.js
+++ index.js
@@ -13,28 +13,34 @@ const port = process.env.PORT || 3000;
 
 console.log("joehoe");
 
-let carrouselWatGroeitEr;
-let activiteiten;
+let dbCarrouselWatGroeitEr;
+let dbActiviteiten;
 
 async function run() {
 	const uri = `mongodb+srv://admin_katrijn:${credentials.password}@wildewei.3etdgyv.mongodb.net/?appName=Wildewei`;
 	const client = new MongoClient(uri);
 
 	const database = client.db("wildewei_databank");
-	carrouselWatGroeitEr = database.collection("watgroeiter");
-	activiteiten = database.collection("activiteiten");
+	dbCarrouselWatGroeitEr = database.collection("watgroeiter");
+	dbActiviteiten = database.collection("activiteiten");
 }
 
 app.get("/watgroeiter", async (req, res) => {
-	const watgroeiterArray = await carrouselWatGroeitEr.find().toArray();
+	const watgroeiterArray = await dbCarrouselWatGroeitEr.find().toArray();
 	console.log(watgroeiterArray);
 	res.send(watgroeiterArray);
 });
 
-app.get("/eersteActiviteit", async (req, res) => {
-	const eersteActiviteit = await activiteiten.find().sort({ startDate: 1 }).limit(1).toArray();
-	console.log(eersteActiviteit);
-	res.send(eersteActiviteit);
+app.get("/activiteiten", async (req, res) => {
+	let activiteiten = dbActiviteiten.find().sort({ startDate: 1 });
+
+	if (req.query.limit) {
+		const limit = Number(req.query.limit);
+		activiteiten = activiteiten.limit(limit);
+	}
+	let activiteitenArray = await activiteiten.toArray();
+	console.log(activiteitenArray);
+	res.send(activiteitenArray);
 });
 
 app.listen(port, () => {
